5831276ff93d430aa2e923f2a46b74c76a9dc3bf
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 15:44:25 +0630

    -

--- NLogExample/Controllers/BlogController.cs
+++ NLogExample/Controllers/BlogController.cs
@@ -3,59 +3,58 @@ using Newtonsoft.Json;
 using NLogExample;
 using NLogExample.Models;
 
-namespace SerilogExample.Controllers
+namespace NLogExample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly ILoggerManager _logger;
+
+    public BlogController(ILoggerManager logger)
     {
-        private readonly ILoggerManager _logger;
-
-        public BlogController(ILoggerManager logger)
-        {
-            _logger = logger;
-        }
-
-        #region Get blogs
-        [HttpGet]
-        public IActionResult GetBlogs()
-        {
-            List<BlogDataModel> lst = new()
-            {
-                new BlogDataModel
-                {
-                    BlogId = Guid.NewGuid(),
-                    BlogTitle = "Blog 1",
-                    BlogAuthor = "Author",
-                    BlogContent = "Content"
-                },
-                new BlogDataModel
-                {
-                    BlogId = Guid.NewGuid(),
-                    BlogTitle = "Blog 2",
-                    BlogAuthor = "Author",
-                    BlogContent = "Content"
-                },
-                 new BlogDataModel
-                {
-                    BlogId = Guid.NewGuid(),
-                    BlogTitle = "Blog 3",
-                    BlogAuthor = "Author",
-                    BlogContent = "Content"
-                }
-            };
-            _logger.LogInfo("Blog list => " + JsonConvert.SerializeObject(lst));
-            return Ok(lst);
-        }
-        #endregion
-
-        #region Test logging
-        [HttpGet("testing")]
-        public IActionResult TestLogging()
-        {
-            _logger.LogDebug("test logging");
-            return Ok();
-        }
-        #endregion
+        _logger = logger;
     }
+
+    #region Get blogs
+    [HttpGet]
+    public IActionResult GetBlogs()
+    {
+        List<BlogDataModel> lst = new()
+        {
+            new BlogDataModel
+            {
+                BlogId = Guid.NewGuid(),
+                BlogTitle = "Blog 1",
+                BlogAuthor = "Author",
+                BlogContent = "Content"
+            },
+            new BlogDataModel
+            {
+                BlogId = Guid.NewGuid(),
+                BlogTitle = "Blog 2",
+                BlogAuthor = "Author",
+                BlogContent = "Content"
+            },
+             new BlogDataModel
+            {
+                BlogId = Guid.NewGuid(),
+                BlogTitle = "Blog 3",
+                BlogAuthor = "Author",
+                BlogContent = "Content"
+            }
+        };
+        _logger.LogInfo("Blog list => " + JsonConvert.SerializeObject(lst));
+        return Ok(lst);
+    }
+    #endregion
+
+    #region Test logging
+    [HttpGet("testing")]
+    public IActionResult TestLogging()
+    {
+        _logger.LogDebug("test logging");
+        return Ok();
+    }
+    #endregion
 }
